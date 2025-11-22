7d94a62c3307018e6a23fb145c65ff214d5f081c
Wannzh <alwanfdhlrhmn@gmail.com>
2025-11-22 23:42:50 +0700

    change layout for admin

--- frontend/zakatnow-frontend/src/components/ProtectedLayout.jsx
+++ frontend/zakatnow-frontend/src/components/ProtectedLayout.jsx
@@ -6,10 +6,10 @@ export default function ProtectedLayout({ children, isAdmin = false }) {
   return (
     <div className="flex min-h-screen">
       {isAdmin ? <Sidebar /> : null}
-      <div className="flex-1 flex flex-col">
+      <div className="flex flex-col flex-1">
         {!isAdmin ? <Navbar /> : null}
         <main className="flex-1">{children}</main>
-        <Footer />
+        {!isAdmin ? null : <Footer />}
       </div>
     </div>
   );
